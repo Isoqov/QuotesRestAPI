54f84f7c64cdf839f6249357c1bc0fa7802f40f2
Boburmirzo <bobur.isokov@spitamen-it.com>
2020-05-07 14:10:37 -0700

    Empty Service created

---
+++ QuotesRestAPI/Service/IDataStorage.cs
@@ -0,0 +1,19 @@
+﻿using QuotesRestAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuotesRestAPI.Service
+{
+    interface IDataStorage
+    {
+        void Edit(int id, QuoteModel quoteModel);
+        void Delete(int id);
+        public List<QuoteModel> Get();
+        public List<QuoteModel> GetByCategory(string category);
+        public QuoteModel GetRandomQuote();
+        void Creat(QuoteModel quote);
+        void Worker();
+    }
+}
---
+++ QuotesRestAPI/Service/Implementation/DataStorage.cs
@@ -0,0 +1,46 @@
+﻿using QuotesRestAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuotesRestAPI.Service.Implementation
+{
+    public class DataStorage : IDataStorage
+    {
+        public void Creat(QuoteModel quote)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(int id, QuoteModel quoteModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<QuoteModel> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<QuoteModel> GetByCategory(string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QuoteModel GetRandomQuote()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Worker()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
