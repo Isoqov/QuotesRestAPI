b106e3fbaa267691c196a0c0ad175a3d16e99cb8
Boburmirzo <bobur.isokov@spitamen-it.com>
2020-05-07 13:57:00 -0700

    Quote model and empty controller created

---
+++ QuotesRestAPI/Model/QuoteModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace QuotesRestAPI.Model
+{
+    public class QuoteModel
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+
+        [JsonPropertyName("Author")]
+        public string Author { get; set; }
+
+        [JsonPropertyName("Quote")]
+        public string Quote { get; set; }
+
+        [JsonPropertyName("Category")]
+        public string Category { get; set; }
+    
+        [JsonIgnore]
+        public DateTime CreatedDate { get; set; }
+    }
+
+        
+}
